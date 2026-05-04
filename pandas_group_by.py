208430e133164f67869f676cb4b5a91c2b58883f
unknown <ahmedmiraz87@gmail.com>
2026-05-04 13:00:09 +0600

    problem solving using groip by

--- pandas_group_by.py
+++ pandas_group_by.py
@@ -5,13 +5,27 @@ file= pd.read_csv("updated_data.csv")
 # print(file.min())
 
 
-instructor_group= file.groupby('instructor')
+instructor_group= file.groupby('instructor')  #grouping row by instructor name
+total_marks_avg=instructor_group['Total Marks'].mean() #avg marks value for each group
 
 for key, instructor in instructor_group:
     print(key)
+    total_marks=instructor['Total Marks']
+    
+
+    file['Total Marks']=file['Total Marks'].fillna(file['instructor'].map(total_marks_avg))
+    # print("Total: ",total_marks)
+    # print("avg :",total_marks_avg)
     # print(instructor)
     # print(instructor.min())
-    print(instructor.describe())
+    # print(instructor.describe())
+    # print(instructor[['key', 'Total Marks', 'Avg']])
+    # print(instructor)
+    # print(file)
+
+
+print(total_marks_avg)
+print(file)
 
 
 
