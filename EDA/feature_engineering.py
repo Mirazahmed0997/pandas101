9fe2085c567465964bf3220636846831b3ad7e7e
unknown <ahmedmiraz87@gmail.com>
2026-05-16 14:54:30 +0600

    first step for feature engineeting

---
+++ EDA/feature_engineering.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+
+
+file = pd.read_csv('titanic.csv')
+df=sns.load_dataset("tips")
+
+# 1- *starter-filterout effective features or delete uneffective col
+#    *split feature cols and target cols
+
+file.drop(['Ticket','Name','PassengerId','Cabin'], axis=1,inplace=True)
+
+x= file.drop(['Survived'], axis=1) # features cols
+y=file['Survived'] # target cols
+
+
+
+# print(x)
+# print(y)
+# print(file.head())
